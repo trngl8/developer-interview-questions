9a37e482d25fde8ecba3e034a28f22a690093c65
Red Rubber Duck <mvch@triangle.software>
2023-10-25 11:25:20 +0300

    twig templates, close #10

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Question;
 use Symfony\Component\Dotenv\Dotenv;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/.env');
@@ -15,4 +17,9 @@ $model = new Question(
 
 $records = $model->getRecords();
 
-echo count($records) . ' records found' . PHP_EOL;
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader, [
+    'cache' => __DIR__ . '/var/cache/twig'
+]);
+
+echo $twig->render('index.html.twig', ['records' => $records]);
