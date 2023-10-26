6d64af3cffbedc5d40fa1e15694cc4c5eaeda9ec
Red Rubber Duck <mvch@triangle.software>
2023-10-26 16:14:56 +0300

    added Database, close #11

---
+++ src/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+class Database
+{
+    private $DB;
+
+    public function __construct(string $dsn, string $username, string $password)
+    {
+        try {
+            $pdoDB = new \PDO($dsn, $username, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+        } catch (\PDOException $e) {
+            die($e->getMessage());
+        }
+        $this->DB = $pdoDB;
+        //echo "Connected to the postgres database successfully!"; //TODO: move to the logger
+    }
+
+    public function getRecords(string $table): array
+    {
+        $sql = sprintf("SELECT * FROM %s", $table);
+        $stmt = $this->DB->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getRecord(string $table, int $id): array
+    {
+        $sql = sprintf("SELECT * FROM %s WHERE id=%d", $table, $id);
+        $stmt = $this->DB->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+}
--- src/Question.php
+++ src/Question.php
@@ -6,22 +6,19 @@ class Question
 {
     private $DB;
 
-    public function __construct(string $dsn, string $username, string $password)
+    public function __construct(Database $pdoDB)
     {
-        try {
-            $pdoDB = new \PDO($dsn, $username, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
-        } catch (\PDOException $e) {
-            die($e->getMessage());
-        }
         $this->DB = $pdoDB;
-        //echo "Connected to the postgres database successfully!"; //TODO: move to the logger
     }
 
-    public function getRecords(): array
+    public function getQuestions(): array
     {
-        $sql = "SELECT * FROM questions";
-        $stmt = $this->DB->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $this->DB->getRecords('questions');
     }
+
+    public function getQuestion(int $id): array
+    {
+        return $this->DB->getRecord('questions', $id);
+    }
+
 }
