dc2f785878667eea017d78f9509c14dd9d2c9e6e
Red Rubber Duck <mvch@triangle.software>
2023-10-28 12:08:51 +0300

    env fixes

--- index.php
+++ index.php
@@ -2,24 +2,21 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use App\DatabaseFactory;
 use App\Question;
-use App\Database;
 use Symfony\Component\Dotenv\Dotenv;
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/.env');
-$db = new Database(
-    sprintf("pgsql:host=localhost;port=5432;dbname=%s;", $_ENV['POSTGRES_DB']),
-    $_ENV['POSTGRES_USER'],
-    $_ENV['POSTGRES_PASSWORD']);
+$db = DatabaseFactory::create($_ENV['APP_ENV']);
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader, [
-    'cache' => __DIR__ . '/var/cache/twig'
+    'cache' => __DIR__ . '/var/cache/twig',
+    'debug' => $_ENV['APP_DEBUG'],
 ]);
 
 $model = new Question($db);
 $records = $model->getQuestions();
-
 echo $twig->render('index.html.twig', ['questions' => $records]);
