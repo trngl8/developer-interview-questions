c37fbd14815529a46b4b7ef658ec0c875946abc9
Red Rubber Duck <mvch@triangle.software>
2023-11-03 19:39:28 +0200

    fixed connections db

--- src/SqliteDatabaseConnection.php
+++ src/SqliteDatabaseConnection.php
@@ -4,5 +4,13 @@ namespace App;
 
 class SqliteDatabaseConnection extends DatabaseConnection
 {
-
+    public function __construct(string $dsn, ?string $username=null, ?string $password=null)
+    {
+        try {
+            $pdoDB = new \PDO($dsn, $username, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+        } catch (\PDOException $e) {
+            throw new \Exception($e->getMessage());
+        }
+        $this->DB = $pdoDB;
+    }
 }
