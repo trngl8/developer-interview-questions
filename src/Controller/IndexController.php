f6c5a97aef19ef7be3446b9c431d6ad83f20b5ef
Red Rubber Duck <mvch@triangle.software>
2023-11-06 12:41:17 +0200

    index controller, close #50

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\NewQuestionType;
+use App\Model;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class IndexController
+{
+    private Environment $twig;
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function index(Request $request, Model $model)
+    {
+        $formFactory = Forms::createFormFactoryBuilder()
+            ->addExtension(new HttpFoundationExtension())
+            ->getFormFactory();
+        $form = $formFactory->create(NewQuestionType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $request->getMethod() === 'POST') {
+
+            $data = $form->getData();
+            if (!$data->title) {
+                $_SESSION['message'] = [
+                    'title' => 'Question title is required',
+                    'type' => 'error',
+                ];
+                return new RedirectResponse('/', Response::HTTP_MOVED_PERMANENTLY);
+            }
+
+            $model->addQuestion([
+                'title' => $data->title,
+                'created_at' => date('Y-m-d H:i:s'),
+            ]);
+            $_SESSION['message'] = [
+                'title' => 'Question added',
+                'type' => 'success',
+            ];
+            return new RedirectResponse('/', Response::HTTP_MOVED_PERMANENTLY);
+        }
+
+        $records = $model->getRecords();
+        $content = $this->twig->render('index.html.twig', [
+            'questions' => $records,
+            'form' => $form->createView(),
+        ]);
+
+        return new Response($content);
+    }
+}
