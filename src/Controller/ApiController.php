d82af640b3d69357f423b69c2e16415498322394
Red Rubber Duck <mvch@triangle.software>
2023-11-09 18:23:33 +0200

    post request

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,6 +10,15 @@ class ApiController
 {
     public function index(Request $request, AbstractModel $model): JsonResponse
     {
+        if ($request->getMethod() === 'POST') {
+            $data = json_decode($request->getContent(), true);
+            $model->addQuestion([
+                'title' => $data['title'],
+                'created_at' => date('Y-m-d H:i:s'),
+            ]);
+            return new JsonResponse(['status' => 'success']);
+        }
+
         return new JsonResponse($model->getRecords());
     }
 }
