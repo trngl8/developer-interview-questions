c8c774213aa61fe9488f0a12ee4d232308cb3676
Red Rubber Duck <mvch@triangle.software>
2023-11-11 14:38:31 +0200

    admin email settings

--- src/Mailer.php
+++ src/Mailer.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mime\Address;
@@ -10,18 +11,22 @@ class Mailer
 {
     private MailerInterface $mailer;
 
-    public function __construct(MailerInterface $mailer)
+    private $logger;
+
+    public function __construct(MailerInterface $mailer, LoggerInterface $logger)
     {
         $this->mailer = $mailer;
+        $this->logger = $logger;
     }
 
     public function sendConfirmEmail(string $email, string $token): void
     {
+        $settings = Core::getMailerSettings();
         $email = (new TemplatedEmail())
-            ->from('test@example.com') //TODO: add custom domain and valid email
+            ->from($settings->adminEmail)
             ->to(new Address($email))
             ->subject('Thanks for signing up!')
-            ->htmlTemplate('emails/signup.html.twig')
+            ->htmlTemplate('emails/confirm.html.twig')
             ->context([
                 'expiration_date' => new \DateTime('+7 days'),
                 'username' => 'foo',
@@ -30,7 +35,7 @@ class Mailer
         try {
             $this->mailer->send($email);
         } catch (\Exception $e) {
-            // TODO: log error and choose alternative transport
+            $this->logger->error($e->getMessage());
             throw new \Exception($e->getMessage());
         }
     }
