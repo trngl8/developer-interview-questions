0b0aa9fa4fb71925ff5e0b16ff0e4b0608f21913
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2023-11-12 21:24:37 +0200

    public folder, close #85

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     plugins: [new miniCssExtractPlugin()],
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'public/dist')
     },
     module: {
         rules: [
